80ca860884c082333d2e1fde66f85232d14dd55b
Ihor Sytnykov <isitnikov@magento.com>
2018-02-26 03:44:52 +0200

    Added untility for updating

---
+++ media/mmcblk0p2/data/usr/bin/getaddon.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ifError()
+{
+    errMsg=$1
+    retVal=$?
+    if [ ! $retVal -eq 0 ]; then
+        echo $errMsg
+    fi
+    exit $retVal
+}
+
+finish()
+{
+    if [ -f "$tmpArchive" ]; then
+        rm "$tmpArchive"
+        echo "Temprary archive $tmpArchive was successfully removed."
+    fi
+}
+trap finish EXIT
+
+tmpArchive="/tmp/xiaofang-addons.tar.gz"
+addonArchive="https://api.github.com/repos/isitnikov/xiaofang-addons/tarball/master"
+echo "Getting an archive from github..."
+wget -O "$tmpArchive" "$addonArchive"
+wait
+#ifError "An error occured. Exit."
+cd /
+ tar --strip-components=1 --exclude=LICENSE --exclude=README.md -xf "$tmpArchive" --directory /media
+ifError "An error occured. Exit."
+echo "Done!"
+exit 0
