c510173a4658be22e9b1839125d3a3c6cebfe36f
Ihor Sytnykov <isitnikov@magento.com>
2018-02-26 03:43:20 +0200

    Some changes

--- media/mmcblk0p2/data/usr/bin/motion_detect.sh
+++ media/mmcblk0p2/data/usr/bin/motion_detect.sh
@@ -1,35 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Copyright 2018 Vladimir Dronnikov
-# GPL
-/media/mmcblk0p2/data/etc/scripts/20-rtsp-server stop
+lifeTime=60
+
+stopServer()
+{
+    /media/mmcblk0p2/data/etc/scripts/20-rtsp-server stop
+}
+
+startServer()
+{
+    /media/mmcblk0p2/data/etc/scripts/20-rtsp-server start
+}
+
+finish()
+{
+    stopServer
+    kill -9 `pidof snx_isp_ctl`
+}
+
+trap finish EXIT
+trap finish TERM
 
 snx_isp_ctl --mdset-thre 1000 # YMMV
-#CTL_PID=$!
 
-#finish()
-#{
-	#kill -9 $MD_PID
-	#kill -9 $CTL_PID
-#	echo 'Finshed!'
-#}
-
-#snx_isp_md 2>&1 | awk '
-#/^Detected Moving!$/ {
-#  if (alarm == 0) {
-#    alarm = 1
-#	system("./motion_action.sh &")
-#  }
-#}
-#/(have no motion status = 0)/ {
-#  if (alarm != 0) {
-#    alarm = 0
-#	system("./motion_noaction.sh &")
-#  }
-#}
-#'
-time=0
-server=0
 snx_isp_md 2>&1 | {
   while IFS= read -r line
   do
@@ -38,37 +31,17 @@ snx_isp_md 2>&1 | {
 		if [ "$time" -lt "$current_time" ]; then
 			time=0
 			server=0
-			/media/mmcblk0p2/data/etc/scripts/20-rtsp-server stop
+			stopServer
 		else
+		    time=$((time + $lifeTime))
 			continue
 		fi
 	fi
 	if [ -z "${line##*ving*}" ]; then
 		server=1
 		time=$(date +"%s")
-		time=$((time + 20))
-		/media/mmcblk0p2/data/etc/scripts/20-rtsp-server start	
+		time=$((time + $lifeTime))
+		startServer
 	fi
   done
-
-  # This won't work without the braces.
-  echo "The last line was: $lastline"
 }
-#MD_PID=$!
-
-#trap finish EXIT
-
-#!/bin/bash
-
-trap_with_arg() {
-    func="$1" ; shift
-    for sig ; do
-        trap "$func $sig" "$sig"
-    done
-}
-
-func_trap() {
-    /media/mmcblk0p2/data/etc/scripts/20-rtsp-server stop
-}
-
-trap_with_arg func_trap INT TERM EXIT
